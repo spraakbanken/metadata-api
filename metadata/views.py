9fac3755972820e0759a1ebfd3caae2a179ed5d9
Anne Schumacher <anne.schumacher@gu.se>
2019-11-05 11:18:19 +0100

    make list of corpora/lexicons, sorted by Swedish name (with Swedish locale)

--- metadata/views.py
+++ metadata/views.py
@@ -1,18 +1,20 @@
 """Collection of routes."""
 
 import os
+import locale
 
 from flask import current_app, jsonify, Blueprint
 import json
 
 general = Blueprint("general", __name__)
+locale.setlocale(locale.LC_ALL, "sv_SE.utf8")
 
 
 @general.route("/")
 def metadata():
     """Return corpus and lexicon meta data as a JSON object."""
-    corpora = read_static_json(current_app.config.get("CORPORA_FILE"))
-    lexicons = read_static_json(current_app.config.get("LEXICONS_FILE"))
+    corpora = dict_to_list(read_static_json(current_app.config.get("CORPORA_FILE")))
+    lexicons = dict_to_list(read_static_json(current_app.config.get("LEXICONS_FILE")))
 
     metadata = {"corpora": corpora, "lexicons": lexicons}
     return jsonify(metadata)
@@ -23,3 +25,8 @@ def read_static_json(jsonfile):
     file_path = os.path.join(current_app.config.get("STATIC"), jsonfile)
     with open(file_path, "r") as f:
         return json.load(f)
+
+
+def dict_to_list(input):
+    """Convert resource dict into list, sorted by name_sv."""
+    return sorted(input.values(), key=lambda x: locale.strxfrm(x.get("name_sv")))
