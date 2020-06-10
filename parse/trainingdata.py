07e21d97a7ed6d0c142d45db5dffdb438ada254c
Arild Matsson <arild.matsson@gu.se>
2020-06-10 21:17:40 +0200

    Flag Svensk Fraktur and Svenska Tidningar as trainingdata

--- parse/trainingdata.py
+++ parse/trainingdata.py
@@ -7,6 +7,8 @@ TRAININGDATA = {
         "suc2",
         "suc3",
         "sv-treebank",
+        "svensk-fraktur-1626-1816",
+        "svenska-tidningar-1818-1870",
         "swe-absa-bank",
         "syntag",
         "talbanken",
