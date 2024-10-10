adf5abd66449230781559753eb3309e3fb192888
Staffan Melin <staffan.melin@svenska.gu.se>
2024-10-10 14:00:48 +0200

    Spelling 'utility.json'

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CORPORA_FILE = "corpus.json"
 LEXICONS_FILE = "lexicon.json"
 MODELS_FILE = "model.json"
 ANALYSES_FILE = "analysis.json"
-UTILITIES_FILE = "utilities.json"
+UTILITIES_FILE = "utility.json"
 RESOURCE_TEXTS_FILE = "resource-texts.json"
 
 NO_CACHE = False  # Set to True to disable caching with memcached
